6f9c092f6ff92938f169eb6432b2ccca94b2a4f5
Omer Zadok <OZadok@gmail.com>
2026-01-30 01:07:40 +0200

    45 degrees rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 _moveDirection;
+    private Vector3 _moveDirection;
     private void OnMove(InputValue value)
     {
-        _moveDirection = value.Get<Vector2>();
-        
+        var moveDirection2D = value.Get<Vector2>();
+        _moveDirection = new Vector3(moveDirection2D.x, 0, moveDirection2D.y);
     }
 
     private void FixedUpdate()
