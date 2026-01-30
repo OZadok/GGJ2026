e6f200d71de1f1674216296b64244c1d9633d50f
Omer Zadok <OZadok@gmail.com>
2026-01-30 18:08:27 +0200

    basic animator for Entity

--- Assets/Scripts/EntityScript.cs
+++ Assets/Scripts/EntityScript.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class EntityScript : MonoBehaviour
 {
+    private static readonly int Action1 = Animator.StringToHash("Action1");
+    private static readonly int Action2 = Animator.StringToHash("Action2");
+    private static readonly int Action3 = Animator.StringToHash("Action3");
     private EquipmentAnchors _equipmentAnchors;
     public Dictionary<ItemType, Item> items =  new Dictionary<ItemType, Item>();
+    
+    private Animator _animator;
 
     private void Awake()
     {
         _equipmentAnchors = GetComponent<EquipmentAnchors>();
+        _animator = GetComponent<Animator>();
     }
 
     public void SetItem(Item item)
@@ -28,15 +34,17 @@ public class EntityScript : MonoBehaviour
     public void DoAction1()
     {
         Debug.Log($"{name} did action 1");
+        _animator.SetTrigger(Action1);
     }
 
     public void DoAction2()
     {
         Debug.Log($"{name} did action 2");
+        _animator.SetTrigger(Action2);
     }
     
     public void DoAction3()
     {
-            
+        _animator.SetTrigger(Action3);
     }
 }
