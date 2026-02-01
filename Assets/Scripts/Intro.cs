baf8f4957bb4027906bcf6ffefc8a446ae93b716
Sol Elan <solipod8@gmail.com>
2026-02-01 19:57:49 +0200

    Fix intro canvas size

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -7,25 +6,39 @@ public class Intro : MonoBehaviour
 {
     private static readonly int Next = Animator.StringToHash("Next");
     private Animator _animator;
+
     [SerializeField] private Transform _anyKeyLable;
+    [SerializeField] private float showAnyKeyAfter = 5f;
+
+    private float _idleTimer;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _anyKeyLable.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        if (Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame
-            || Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame)
+        bool inputPressed =
+            (Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame) ||
+            (Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame);
+
+        if (inputPressed)
         {
-            if (_anyKeyLable.gameObject.activeSelf) { _anyKeyLable.gameObject.SetActive(false); }
+            _idleTimer = 0f;
+            _anyKeyLable.gameObject.SetActive(false);
 
             _animator.SetTrigger(Next);
+
             if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Intro 6"))
-            {
-                SceneManager.LoadScene($"SampleScene");
-            }
+                SceneManager.LoadScene("SampleScene");
+
+            return;
         }
+
+        _idleTimer += Time.deltaTime;
+        if (_idleTimer >= showAnyKeyAfter && !_anyKeyLable.gameObject.activeSelf)
+            _anyKeyLable.gameObject.SetActive(true);
     }
 }
